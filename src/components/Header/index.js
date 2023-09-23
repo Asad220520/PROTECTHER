979c5b951b91e8738c2c9b1d60c3cd1df4b6c8b3
Asadhkm <asadhkm4@gmail.com>
2023-09-23 20:52:47 +0600

    PETICIA

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+const Header = () => {
+  return (
+    <div id="header">
+      <div className="container">
+        <div className="header">header</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
