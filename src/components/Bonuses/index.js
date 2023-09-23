3d0118bd3abf3504ddc265a03def67bdeb83da83
Asadhkm <asadhkm4@gmail.com>
2023-09-24 03:01:35 +0600

    add

--- src/components/Bonuses/index.js
+++ src/components/Bonuses/index.js
@@ -12,13 +12,14 @@ const Bonuses = () => {
         <div className="bonuses">
           <div className="bonuses__text">
             <h1>
-              What do <span>you</span> want to change today?
+              Мы делаем проблему домашнего насилия видимой и поддерживаем тех,
+              кому нужна наша помощь
             </h1>
             <p>
-              38 Degrees is a community of more than a million people who – in a
-              moment away from their busy days – take small actions on issues
-              they care about, which all add up to something bigger, a movement
-              for a better Britain for everyone who calls our country home.
+              <span>«MyLaw»</span>— это десятки людей, которых объединяет
+              желание изменить отношение к проблеме домашнего насилия и помочь
+              тем, кто пострадал от него. Мы работаем благодаря поддержке наших
+              доноров.
             </p>
             <div className="bonuses__text-btn">
               <button>See our impact</button>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,25 @@ import React from "react";
 import "./index.scss";
 import { Link, NavLink } from "react-router-dom";
 import { BiSolidUserCircle } from "react-icons/bi";
-
+import logo from "../../image/MyLaw.svg";
 const Header = () => {
   return (
     <div id="header">
       <div className="container">
         <div className="header">
-          <h1>logo</h1>
+          <Link>
+            <img width={150} src={logo} alt="" />
+          </Link>
           <nav className="nav">
-            <NavLink to={"/create"}>Создать петицию</NavLink>
+            <NavLink to={"/create"}>Мне нужна помощь</NavLink>
+            <NavLink to={"/create"}>ПРО Насилие</NavLink>
+            <NavLink to={"/create"}>Карта центров помощи</NavLink>
+            <NavLink to={"/create"}>О нас</NavLink>
+            <NavLink to={"/create"}>Обучение</NavLink>
+            <NavLink to={"/create"}>SOS-размещение</NavLink>
+            {/* <NavLink to={"/create"}>Создать петицию</NavLink>
             <NavLink to={"/myPet"}>Мои петиции</NavLink>
-            <NavLink to={"/seePet"}>Просмотреть петиции</NavLink>
+            <NavLink to={"/seePet"}>Просмотреть петиции</NavLink> */}
             <div className="nav__btn">
               <Link to={"/auth"}>
                 <BiSolidUserCircle style={{ fontSize: "28px" }} />
