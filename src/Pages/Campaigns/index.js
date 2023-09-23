3afac92f54498900da339c59fde7e72506827c84
Asadhkm <asadhkm4@gmail.com>
2023-09-23 22:13:47 +0600

    Path

---
+++ src/Pages/Campaigns/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Campaigns = () => {
+  return (
+    <div id="campaigns">
+      <div className="container">
+        <div className="campaigns">campaigns</div>
+      </div>
+    </div>
+  );
+};
+
+export default Campaigns;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import "./index.scss";
+import { Link, NavLink } from "react-router-dom";
+import { BiSolidUserCircle } from "react-icons/bi";
+
 const Header = () => {
   return (
     <div id="header">
       <div className="container">
-        <div className="header">header</div>
+        <div className="header">
+          <h1>logo</h1>
+          <nav className="nav">
+            <NavLink to={"/campaigns"}>Campaigns</NavLink>
+            <NavLink to={"/latest"}>Latest</NavLink>
+            <NavLink to={"/about"}>About</NavLink>
+            <NavLink to={"/donate"}>Donate</NavLink>
+            <div className="nav__btn">
+              <Link>
+                <BiSolidUserCircle style={{ fontSize: "28px" }} />
+              </Link>
+              <button>Start a Petition</button>
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   );
